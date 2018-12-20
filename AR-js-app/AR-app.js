6b8500535b3fc2181bc6c5e335a520a747813a8f
Steven Tu <stu562@gmail.com>
2018-12-20 20:50:43 +0800

    armarker overlay

--- AR-js-app/AR-app.js
+++ AR-js-app/AR-app.js
@@ -1,23 +1,51 @@
 $(document).ready(function(){
+  //user defined values 
 
-	//https://raw.githubusercontent.com/akashkuttappa/armodels/master/markers/hiro.jpg
-	//access the marker either on computer or mobilephone and place infront of webcam or frontfacing camera
-	//creates the button for main screen to enter the AR App 
+  //on click functions 
+  $('.btn-obj1').on('click', function(e){
+    //should append html to show box on top of marker 
+  	// adds object or changes the current object 
+  	// adds text 
+    // <a-marker preset='hiro'>
+    //   <a-box position='0 0.5 0' material='opacity: 0.5; side: double;color:red;'>
+    //     <a-torus-knot radius='0.26' radius-tubular='0.05'>
+    //       <a-animation attribute="rotation" to="360 0 0" dur="5000" easing='linear' repeat="indefinite"></a-animation>
+    //     </a-torus-knot>
+    //   </a-box>
+    // </a-marker>
+    console.log(e)
+    alert('i was clicked')
+            // $('.objects').text('say hi');
+            //$.appendTo($())
 
+  	});
+  $('.btn-obj2').on('click', function(){
+  	//opens up setting 
+    // plays sound 
+    /*
+    <a-marker preset='hiro'>
+      <a-box position='0 0.5 0' material='opacity: 0.5; side: double;color:red;'>
+        <a-torus-knot radius='0.26' radius-tubular='0.05'>
+          <a-animation attribute="rotation" to="360 0 0" dur="5000" easing='linear' repeat="indefinite"></a-animation>
+        </a-torus-knot>
+      </a-box>
+    </a-marker>
 
-
-	//creates users defined object on click - a square , teeth model, corgi
-
-
-	//js functions 
-
-	//on click events 
-	$('.btn').on('click', function(){	});//selects, applies object 1 box 
-  	$('.btn').on('click', function(){	});// selects, applies object2 teeth
-  	$('.btn').on('click', function(){	});//selects, applies object 3 corgi 
-
-  	//get 3d models from google blocks 
-
-
-	//fps? shows up in the html? 
+    */
+  	
+  	});
+  $('.btn-obj3').on('click', function(){
+    //opens up setting 
+    /*
+    <a-marker preset='hiro'>
+      <a-box position='0 0.5 0' material='opacity: 0.5; side: double;color:red;'>
+        <a-torus-knot radius='0.26' radius-tubular='0.05'>
+          <a-animation attribute="rotation" to="360 0 0" dur="5000" easing='linear' repeat="indefinite"></a-animation>
+        </a-torus-knot>
+      </a-box>
+    </a-marker>
+    */
+    
+    });
 });
+
