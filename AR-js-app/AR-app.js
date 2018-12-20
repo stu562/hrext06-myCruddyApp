7a629a78837f9886f21954668dd64e8d27571848
Steven Tu <stu562@gmail.com>
2018-12-21 02:49:47 +0800

    fix bug with jquery; working ar with screen overlay

--- AR-js-app/AR-app.js
+++ AR-js-app/AR-app.js
@@ -1,51 +1,17 @@
 $(document).ready(function(){
-  //user defined values 
+  //user defined values
+  //error with jquery script 
+
 
   //on click functions 
-  $('.btn-obj1').on('click', function(e){
-    //should append html to show box on top of marker 
-  	// adds object or changes the current object 
-  	// adds text 
-    // <a-marker preset='hiro'>
-    //   <a-box position='0 0.5 0' material='opacity: 0.5; side: double;color:red;'>
-    //     <a-torus-knot radius='0.26' radius-tubular='0.05'>
-    //       <a-animation attribute="rotation" to="360 0 0" dur="5000" easing='linear' repeat="indefinite"></a-animation>
-    //     </a-torus-knot>
-    //   </a-box>
-    // </a-marker>
-    console.log(e)
-    alert('i was clicked')
-            // $('.objects').text('say hi');
-            //$.appendTo($())
-
+  $('.btn-obj1').on('click', function(){
+    $('objectselector').appendTo("<a-box position='0 0.5 0' material='color: orange;'></a-box>");
   	});
   $('.btn-obj2').on('click', function(){
-  	//opens up setting 
-    // plays sound 
-    /*
-    <a-marker preset='hiro'>
-      <a-box position='0 0.5 0' material='opacity: 0.5; side: double;color:red;'>
-        <a-torus-knot radius='0.26' radius-tubular='0.05'>
-          <a-animation attribute="rotation" to="360 0 0" dur="5000" easing='linear' repeat="indefinite"></a-animation>
-        </a-torus-knot>
-      </a-box>
-    </a-marker>
-
-    */
-  	
+  	$('objectselector').appendTo("<a-sphere position='0 0.5 0' material='color: orange;'></a-box>")
   	});
   $('.btn-obj3').on('click', function(){
-    //opens up setting 
-    /*
-    <a-marker preset='hiro'>
-      <a-box position='0 0.5 0' material='opacity: 0.5; side: double;color:red;'>
-        <a-torus-knot radius='0.26' radius-tubular='0.05'>
-          <a-animation attribute="rotation" to="360 0 0" dur="5000" easing='linear' repeat="indefinite"></a-animation>
-        </a-torus-knot>
-      </a-box>
-    </a-marker>
-    */
-    
+    $('objectselector').appendTo("<a-plane position='0 0.5 0' material='color: orange;'></a-box>")
     });
 });
 
---
+++ AR-js-app/README.md
@@ -0,0 +1,21 @@
+### Read me: AR-js-app things to do 
+
+- [x] explore AR.js using marker base A-frame 
+	[x] three.js /threeAr.js
+	[ ] artoolkit
+- [x] get the simple box object to work 
+- [x] change the object to box 
+	 
+- [x] obtain object 3D models 
+- [ ] populate object on click in AR space 
+- [x] add different objects button
+- [ ] button should change object
+- [ ] objects should move 
+- [ ] objects should be able to interacted with 
+
+
+- [ ] Optional: change to different marker than the Hiro marker
+- [ ] add in FPS reader 
+- [ ] change the marker to follow the users face instead 
+- [ ] improve UI
+	- [ ] add a button for the main page to load the app  
