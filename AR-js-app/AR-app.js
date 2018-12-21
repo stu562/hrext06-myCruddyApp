a58af4838c506b7938ae35533214ed96fb3e65e5
Steven Tu <stu562@gmail.com>
2018-12-21 08:15:27 +0800

    cleaning up unused code

--- AR-js-app/AR-app.js
+++ AR-js-app/AR-app.js
@@ -1,37 +1,22 @@
 $(document).ready(function(){
-  //user defined values
-  //error with jquery script 
   var getRandomInt = function (min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
-  }//random interger
-
+  }
   var getRandomColor = function(){
       var red = getRandomInt(0, 255);
       var green = getRandomInt(0, 255);
       var blue = getRandomInt(0, 255);    
       return "rgb(" + red + "," + green + "," + blue + ")"; //'rgb(#r, #g, #b)'
   }
-  
   //on click functions 
-  $(".btn-obj1").click( function(e){
-    var currentColor = getRandomColor()
-
-    $(".currentObj").replaceWith("<a-box class='currentObj' position='0 0.5 0' material='color: " + currentColor + ";'></a-box>");
-    // $('objectselector').appendTo("<a-box position='0 0.5 0' material='color: orange;'></a-box>");
+  $(".btn-obj1").click( function(){
+    $(".currentObj").replaceWith("<a-box class='currentObj' position='0 0.5 0' material='color: " + getRandomColor() + ";'></a-box>");
   	});
-  $(".btn-obj2").click( function(e){
-    var currentColor = getRandomColor()
-
-    $(".currentObj").replaceWith("<a-sphere class='currentObj' position='0 0.5 0' material='color: " + currentColor + ";'></a-box>");
-    // $('objectselector').appendTo("<a-box position='0 0.5 0' material='color: orange;'></a-box>");
-
-    // alert(currentColor + 'applied')
+  $(".btn-obj2").click( function(){
+    $(".currentObj").replaceWith("<a-sphere class='currentObj' position='0 0.5 0' material='color: " + getRandomColor() + ";'></a-box>");
     });
-  $(".btn-obj3").click( function(e){
-    var currentColor = getRandomColor()
-
-    $(".currentObj").replaceWith("<a-plane class='currentObj'position='0 0.5 0' material='color: " + currentColor + ";'></a-box>");
-    // $('objectselector').appendTo("<a-box position='0 0.5 0' material='color: orange;'></a-box>");
+  $(".btn-obj3").click( function(){
+    $(".currentObj").replaceWith("<a-plane class='currentObj'position='0 0.5 0' material='color: " + getRandomColor() + ";'></a-box>");
     });
 });
 
--- AR-js-app/README.md
+++ AR-js-app/README.md
@@ -1,16 +1,30 @@
 ### Read me: AR-js-app things to do 
+- get a hiro marker on phone or computer 
+AR libraries are the work of Jeromeetienne
+- https://github.com/jeromeetienne/AR.js
 
+******** pre-material *********************
 - [x] explore AR.js using marker base A-frame 
 	[x] three.js /threeAr.js
 	[ ] artoolkit
+
+	 
+******* Milestones ************************
 - [x] get the simple box object to work 
 - [x] change the object to box 
-	 
-- [x] obtain object 3D models 
 - [x] populate object on click in AR space 
 - [x] add different objects button
 - [x] button should change object color
-- [ ] objects should move 
+- [x] object should be able to change to newly selected option
+
+
+********* Future Todo **********************
+- [ ] resize function
+- [ ] move navigation bar to leftside 
+- [ ] add full screen mode 
+- [ ] replace objects with artist 3D models 
+- [ ] objects should move periodicall with a jump effect 
+- [ ] objects should get bigger or smaller based on click event 
 - [ ] objects should be able to interacted with 
 
 
